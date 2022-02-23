9cd369a6c1897b2f9dbf1dac662f63a9cf4c79fd
rohanthapa123 <98463872+rohanthapa123@users.noreply.github.com>
2022-02-23 20:56:39 +0545

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const logo = document.querySelector(".logo");
 const rohan = document.querySelector(".rohan");
+const container1 = document.querySelector(".container");
+const about = document.querySelector(".h1about");
+const aboutme = document.querySelector(".aboutme");
 
 burgerBtn.addEventListener("click", ()=>{
     navLinks.classList.toggle("nav-active");
@@ -18,6 +21,9 @@ logo.addEventListener("click", ()=>{
     rohan.classList.toggle("logo-active");
 });
 
+about.addEventListener("click", ()=>{
+    about.classList.toggle("h1about-active");
+})
 
 
 
